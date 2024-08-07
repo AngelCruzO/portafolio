b6325ffee4ec9e3e9f11b20421d1b72f90e782df
AngelCruzO <vce08201@innovaccion.mx>
2024-08-07 09:57:23 -0600

    update logic.js

--- js/logic.js
+++ js/logic.js
@@ -13,3 +13,19 @@ let header = document.querySelector('header');
 menuToggle.onclick = function(){
   header.classList.toggle('active');
 }
+
+const container = document.querySelector('.slide-track');
+
+const images = ['Azure','C','C++','CSS3','Git','GitHub','HTML5','Markdown','Materialize','MATLAB','MySQL','PHP','Postman','Python','SQLServer','Vim','VS','VSCode'];
+
+images.forEach(image => {
+  const slideDiv = document.createElement('div');
+  slideDiv.classList.add('slide');
+
+  const imgElement = document.createElement('img');
+
+  imgElement.src = `assets/images/tech/${image}.svg`;
+
+  slideDiv.appendChild(imgElement);
+  container.appendChild(slideDiv);
+});
