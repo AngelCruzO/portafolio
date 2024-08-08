a146f6c14cf3c4f492c7485b389309009c2f7be6
AngelCruzO <vce08201@innovaccion.mx>
2024-08-08 11:15:00 -0600

    update slide logos and add animation rotate

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,7 @@
 let list = document.querySelectorAll('.nav li');
 
 function active(){
-  list .forEach((i) => i.classList.remove('active'));
+  list.forEach((i) => i.classList.remove('active'));
   this.classList.add('active');
 }
 
@@ -14,18 +14,19 @@ menuToggle.onclick = function(){
   header.classList.toggle('active');
 }
 
-const container = document.querySelector('.slide-track');
+const container = document.querySelector('.logos-slide');
 
 const images = ['Azure','C','C++','CSS3','Git','GitHub','HTML5','Markdown','Materialize','MATLAB','MySQL','PHP','Postman','Python','SQLServer','Vim','VS','VSCode'];
 
 images.forEach(image => {
-  const slideDiv = document.createElement('div');
-  slideDiv.classList.add('slide');
 
   const imgElement = document.createElement('img');
 
   imgElement.src = `assets/images/tech/${image}.svg`;
 
-  slideDiv.appendChild(imgElement);
-  container.appendChild(slideDiv);
+  container.appendChild(imgElement);
 });
+
+var copy = container.cloneNode(true);
+document.querySelector('.logos').appendChild(copy);
+
