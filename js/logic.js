944f7e09ce77496ebae86c4ed1d212f852d9e276
AngelCruzO <vce08201@innovaccion.mx>
2024-08-18 09:38:16 -0600

    Fix prueba json2

--- js/logic.js
+++ js/logic.js
@@ -18,27 +18,32 @@ const container = document.querySelector('.logos-slide');
 
 /*const images = ['Azure','C','C++','CSS3','Git','GitHub','HTML5','Markdown','Materialize','MATLAB','MySQL','PHP','Postman','Python','SQLServer','Vim','VS','VSCode'];*/
 
-images.forEach(image => {
 
- 
 fetch('js/config.json').then(response => {
   if(!response.ok){
     throw new Error("La respuesta no fue satisfactoria");
   }
   return response.json();
 }).then( data => {
+  
   console.log(data);
+
   const images = data[0].images
- const imgElement = document.createElement('img');
 
-  imgElement.src = `assets/images/tech/${image}.svg`;
-  imgElement.alt = `logo ${image}`
+  images.forEach(image => {
 
-  container.appendChild(imgElement);
+    const imgElement = document.createElement('img');
+
+    imgElement.src = `assets/images/tech/${image}.svg`;
+    imgElement.alt = `logo ${image}`
+
+    container.appendChild(imgElement);
 });
 
-var copy = container.cloneNode(true);
-document.querySelector('.logos').appendChild(copy);
+  var copy = container.cloneNode(true);
+  document.querySelector('.logos').appendChild(copy);
+
+
 
 }).catch(error => {
   console.error('Hay un error en la operación fetch:', error);
