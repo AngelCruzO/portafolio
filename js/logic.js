83190a5240dcbb505c415c581f8f6b24fc7d1fa9
AngelCruzO <vce08201@innovaccion.mx>
2024-08-18 09:31:14 -0600

    test images slider

--- js/logic.js
+++ js/logic.js
@@ -16,11 +16,20 @@ menuToggle.onclick = function(){
 
 const container = document.querySelector('.logos-slide');
 
-const images = ['Azure','C','C++','CSS3','Git','GitHub','HTML5','Markdown','Materialize','MATLAB','MySQL','PHP','Postman','Python','SQLServer','Vim','VS','VSCode'];
+/*const images = ['Azure','C','C++','CSS3','Git','GitHub','HTML5','Markdown','Materialize','MATLAB','MySQL','PHP','Postman','Python','SQLServer','Vim','VS','VSCode'];*/
 
 images.forEach(image => {
 
-  const imgElement = document.createElement('img');
+ 
+fetch('js/config.json').then(response => {
+  if(!response.ok){
+    throw new Error("La respuesta no fue satisfactoria");
+  }
+  return response.json();
+}).then( data => {
+  console.log(data);
+  const images = data[0]['images']
+ const imgElement = document.createElement('img');
 
   imgElement.src = `assets/images/tech/${image}.svg`;
   imgElement.alt = `logo ${image}`
@@ -31,13 +40,6 @@ images.forEach(image => {
 var copy = container.cloneNode(true);
 document.querySelector('.logos').appendChild(copy);
 
-fetch('js/config.json').then(response => {
-  if(!response.ok){
-    throw new Error("La respuesta no fue satisfactoria");
-  }
-  return response.json();
-}).then( data => {
-  console.log(data);
 }).catch(error => {
   console.error('Hay un error en la operación fetch:', error);
 });
