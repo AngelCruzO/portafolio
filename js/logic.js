0259ebe3dbb6faf7c6b3cc3824e9f31dd05e02b1
AngelCruzO <vce08201@innovaccion.mx>
2024-08-18 10:10:19 -0600

    add card with js

--- js/logic.js
+++ js/logic.js
@@ -14,7 +14,6 @@ menuToggle.onclick = function(){
   header.classList.toggle('active');
 }
 
-const container = document.querySelector('.logos-slide');
 
 /*const images = ['Azure','C','C++','CSS3','Git','GitHub','HTML5','Markdown','Materialize','MATLAB','MySQL','PHP','Postman','Python','SQLServer','Vim','VS','VSCode'];*/
 
@@ -27,8 +26,11 @@ fetch('js/config.json').then(response => {
 }).then( data => {
   
   console.log(data);
+  const container = document.querySelector('.logos-slide');
+  const contentProjects = document.querySelector('#projects .content');
 
   const images = data[0].images
+  const projects = data[0].projects
 
   images.forEach(image => {
 
@@ -43,7 +45,34 @@ fetch('js/config.json').then(response => {
   var copy = container.cloneNode(true);
   document.querySelector('.logos').appendChild(copy);
 
+  for(const key in projects){
+    if(projects.hasOwnProperty(key)){
+      const project = projects[key];
 
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      let cardContent = '<img src="assets/images/default.jpg" alt="Default image">';
+      cardContent += '<div class="card-content">';
+      cardContent += '<div class="card-title"><h3>${project.name}</h3></div>';
+      cardContent += '<div class="card-description"><p>${project.description}</p></div>';
+      cardContent += '<div class="card-footer">';
+
+      if(project.web){
+	cardContent += '<a class="btn" href="${project.web}" target="_blank">Demo</a>';
+      }
+
+      if(project.github){
+	cardContent += '<a class="btn" href="${project.github}" target="_blank">Github</a>';
+      }
+
+      cardContent += '</div></div>';
+
+      card.innerHTML = cardContent;
+
+      contentProjects.appendChild(card);
+    }
+  }
 
 }).catch(error => {
   console.error('Hay un error en la operación fetch:', error);
